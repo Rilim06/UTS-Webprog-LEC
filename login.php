9fee4c3d92531c2df4416c13988b93225bc7b56e
Rilim06 <r.marvinlim@gmail.com>
2023-10-12 13:41:09 +0700

    Captcha Added

--- login.php
+++ login.php
@@ -9,11 +9,46 @@
         Please fill in all the fields.
     </div>
 <?php endif; ?>
+<?php if (isset($_GET['captcha'])): ?>
+    <div class="alert alert-danger text-center w-80 mx-auto fw-bold" role="alert">
+        Captcha incorrect.
+    </div>
+<?php endif; ?>
 
 <form action="login_process.php" method="POST">
+    <label>Username : </label>
     <input type="text" name="username" placeholder="Username" /><br />
+    <label>Password : </label>
     <input type="password" name="password" placeholder="Password" /><br />
+    <?php $captcha = generateCaptcha() ?>
+    <label>Captcha : </label>
+    <input type="text" name="captcha" placeholder="Enter Captcha" />
+    <label>
+        <?php echo $captcha; ?>
+    </label>
+    <input type="hidden" name="captcha_generate" value="<?php echo $captcha; ?>" /><br />
     <button type="submit">Login</button>
 </form>
 <p>Don't have an account?</p>
 <a href="register.php">Register Now</a>
+
+<?php
+function generateCaptcha($length = 5)
+{
+    $numbers = '0123456789';
+    $letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    $randomNumber = $numbers[rand(0, strlen($numbers) - 1)];
+
+    $randomLetters = '';
+    for ($i = 0; $i < ($length - 1); $i++) {
+        $randomLetters .= $letters[rand(0, strlen($letters) - 1)];
+    }
+
+    $randomCaptcha = $randomNumber . $randomLetters;
+    $randomCaptcha = str_shuffle($randomCaptcha);
+
+    return $randomCaptcha;
+}
+
+?>
--- login_process.php
+++ login_process.php
@@ -10,10 +10,12 @@ $stmt = $db->prepare($sql);
 $stmt->execute([$username]);
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if ($_POST['username'] == NULL || ($_POST['password'] == NULL)) {
+if ($_POST['username'] == NULL || $_POST['password'] == NULL || $_POST['captcha'] == NULL) {
     header("Location: login.php?error=error");
 } elseif (!password_verify($password, $row["password"])) {
     header("Location: login.php?empty=error");
+} elseif ($_POST['captcha'] !== $_POST['captcha_generate']) {
+    header("Location: login.php?captcha=error");
 } else {
     $_SESSION["id"] = $row["id"];
     $_SESSION["username"] = $row["username"];
